8971ab12a2128a8cb1ca5e93301026d98e5a04e9
joaopaulofg <joao.gomes@ccc.ufcg.edu.br>
2024-12-04 22:21:32 -0300

    fucionalidade de edicao ok

--- src/app/post/edit/edit.component.ts
+++ src/app/post/edit/edit.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { Post } from '../post';
+import { PostService } from '../post.service';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.css'
 })
 export class EditComponent {
+  id!: number;
+  post!: Post;
+  form!: FormGroup;
 
+  constructor(
+    public postService: PostService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['postId'];
+    this.postService.find(this.id).subscribe((data: Post) => {
+      this.post = data;
+    });
+
+    this.form = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      body: new FormControl('', Validators.required)
+    });
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  submit() {
+    console.log(this.form.value);
+    this.postService.update(this.id, this.form.value).subscribe((res: any) => {
+      console.log('Atualizacao do post com id ' + this.id + ' realizada com sucesso!');
+      this.router.navigateByUrl('post/index');
+    });
+  }
 }
